232a3f80118f12c6c15280664213ea40f14326af
lalakii <sa@iamverylovely.com>
2024-01-23 07:36:04 +0800

    Get_event_build

---
+++ README.md
@@ -0,0 +1,11 @@
+# Android GetEvent
+
+[English](README_en.md) | [中文](README.md)
+
+**通过Shizuku权限，读取触摸屏事件示例**  [下载演示APP](https://github.com/lalakii/get_event/releases/)
+
+读取到的数据还需要解析，我只实现到这一步，有功能需求自行完善。
+
+<img src="./video/demo.gif" width="300"/>
+
+## By lalaki.cn
---
+++ app/src/main/java/cn/lalaki/touch_event/MainActivity.java
@@ -0,0 +1,110 @@
+package cn.lalaki.touch_event;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.common.io.ByteStreams;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import rikka.shizuku.Shizuku;
+
+public class MainActivity extends Activity implements View.OnClickListener, Shizuku.OnRequestPermissionResultListener {
+    private boolean permissionIsGranted = false;
+    private TextView te_data;
+    private File rish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        te_data = findViewById(R.id.touch_data);
+        File external = getFilesDir();
+        if (external != null) {
+            String path = external.getAbsolutePath() + "/private/";
+            if (new File(path).mkdirs()) {
+                Toast.makeText(this, "create private dir is done.", Toast.LENGTH_SHORT).show();
+            }
+            rish = new File(path + "rish");
+            if (!rish.exists()) {
+                String[] files = {"rish_shizuku.dex", "rish"};
+                for (String name : files) {
+                    try (InputStream rish_dex = getAssets().open(name)) {
+                        try (FileOutputStream out = new FileOutputStream(path + name)) {
+                            ByteStreams.copy(rish_dex, out);
+                        }
+                    } catch (IOException ignored) {
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        Intent shizukuIntent = getPackageManager().getLaunchIntentForPackage("moe.shizuku.privileged.api");
+        if (shizukuIntent != null) {
+            if (Shizuku.pingBinder()) {
+                permissionIsGranted = Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED;
+                if (permissionIsGranted) {
+                    findViewById(R.id.tips).setAlpha(0F);
+                } else {
+                    Shizuku.addRequestPermissionResultListener(this);
+                    Shizuku.requestPermission(0);
+                }
+            }
+        }
+        super.onResume();
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, int grantResult) {
+        permissionIsGranted = grantResult == 0;
+    }
+
+    private InputStream in;
+    private String line;
+    private boolean isRunning = false;
+    private final ProcessBuilder builder = new ProcessBuilder();
+
+    @Override
+    public void onClick(View view) {
+        if (!permissionIsGranted) return;
+        if (isRunning) {
+            Toast.makeText(this, R.string.end, Toast.LENGTH_SHORT).show();
+            finish();
+            System.exit(0);
+        }
+        if (rish.exists()) {
+            isRunning = !isRunning;
+            Toast.makeText(this, R.string.touch_tips, Toast.LENGTH_SHORT).show();
+            new Thread(() -> {
+                try {
+                    in = builder.command("sh", rish.getAbsolutePath(), "-c", "getevent -t").start().getInputStream();
+                    new Thread(() -> {
+                        while (in != null) {
+                            try {
+                                byte[] data = new byte[2048];
+                                ByteStreams.readFully(in, data);
+                                line = new String(data);
+                                runOnUiThread(() -> te_data.setText(line));
+                            } catch (IOException e) {
+                                runOnUiThread(() -> te_data.setText(e.getLocalizedMessage()));
+                            }
+                        }
+                    }).start();
+                } catch (IOException e) {
+                    runOnUiThread(() -> te_data.setText(e.getLocalizedMessage()));
+                }
+            }).start();
+        }
+    }
+}
